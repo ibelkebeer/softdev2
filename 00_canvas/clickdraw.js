29bc1f76ac9da8a09be91ee1697be4354b768877
ibelkebeer <ibelkebir@stuy.edu>
2019-01-30 13:16:19 -0500

    started canvas

---
+++ 00_canvas/clickdraw.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("slate");
+var clear = document.getElementById("clear");
+var rect = document.getElementById("rect");
+var dot = document.getElementById("dot");
+canvas.addEventListener('click', function(){
+});
+clear.addEventListener('click', function(){
+});
+rect.addEventListener('click', function(){
+});
+dot.addEventListener('click', function(){
+});
