8713009c1d907edf7fbc178219736f516a553fb0
ibelkebeer <ibelkebir@stuy.edu>
2019-01-31 13:08:41 -0500

    Simpler

---
+++ 01_canvas/clickdraw.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
+var bound = canvas.getBoundingClientRect();
+
+var clear = document.getElementById("clear");
+var toggle = document.getElementById("toggle");
+
+var mode = true;
+
+canvas.addEventListener('click', function(e){
+    if(mode){
+	ctx.fillStyle = "#18f0f0";
+	ctx.beginPath();
+	ctx.ellipse(e.clientX - bound.left, e.clientY - bound.top, 50, 50, 0, 0, 2 * Math.PI);
+	ctx.fill();
+    }else{
+	ctx.fillStyle = "#af33f2";
+	ctx.fillRect(e.clientX - bound.left, e.clientY - bound.top, 80, 80);
+    }
+});
+
+clear.addEventListener('click', function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+});
+
+toggle.addEventListener('click', function(){
+    if(mode){
+	mode = false;
+    }else{
+	mode = true;
+    }
+});
