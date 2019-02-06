46f46a30b8cb3595b817b78fd93be046084e3cb3
Imad <ibelkebir@stuy.edu>
2019-02-05 20:53:17 -0500

    gotta go fast

--- 03_canvas_anim/anim.js
+++ 03_canvas_anim/anim.js
@@ -1,7 +1,7 @@
-//Team MS Paint: Dennis Chen Imad Belkebir
+//Team MS Paint: Isaac Jon Imad Belkebir
 //SoftDev pd7
-//K #02: Connecting the dots
-//2/1/19
+//K #03: They lock us in the tower whenever we get caught
+//2/4/19
 
 var canvas = document.getElementById('playground');
 var start = document.getElementById('start');
@@ -27,11 +27,13 @@ function paint() {
   //console.log(radius);
   if(animating){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(radius > 0){
     	ctx.beginPath();
     	ctx.fillStyle = "#18f0f0";
     	ctx.ellipse(300, 300, radius, radius, 0, 0, 2 * Math.PI);
     	ctx.fill()
     	ctx.stroke();
+    }
       if(radius == 0){
         radius++;
         growing = true;
@@ -43,9 +45,9 @@ function paint() {
         growing = true;
       }
       if(growing){
-        radius++;
+        radius += 5;
       }else{
-        radius--;
+        radius -= 5;
       }
       window.requestAnimationFrame(paint);
     }else{
