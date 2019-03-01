4dc135468ace0f51d3fc2a1cc153a2d95d2964f8
Imad <ibelkebir@stuy.edu>
2019-03-01 14:47:47 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -3,9 +3,10 @@ import pymongo
 import os
 
 os.system("sudo mv pkmn.json ../../data/db")
-os.system("mongod -v --bind_ip_all --noauth --dbpath ~/data/db")
-os.system("mongoimport --db YaThatWasABanana --collection pkmn --drop --file ../../data/db/pkmn.json")
+os.system("mongoimport --db YaThatWasABanana --collection pkmn --drop --file ~/data/db/pkmn.json")
 SERVER_ADDR = "206.81.13.98"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.YaThatWasABanana
 collection = db.pkmn
+
+print(list(collection.find({"pokemon.id": 1})))
