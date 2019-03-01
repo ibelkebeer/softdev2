d6cfa612f437d1ae1aea5929d3e7455691d54ecb
Imad <ibelkebir@stuy.edu>
2019-03-01 14:30:16 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -1,11 +1,8 @@
-#Team YaThatWasABanana -- 
-
-
-
+#Team YaThatWasABanana --
 import pymongo
 import os
 
-os.system("mv pkmn.json ../../data/db")
+os.system("sudo mv pkmn.json ../../data/db")
 os.system("mongod -v --bind_ip_all --noauth --dbpath ../../data/db")
 os.system("mongoimport --db YaThatWasABanana --collection pkmn --drop --file ../../data/db/pkmn.json")
 SERVER_ADDR = "206.81.13.98"
