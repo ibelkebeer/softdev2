f5607895fa76defdd14d54d2ab1aafc630360512
Imad <ibelkebir@stuy.edu>
2019-03-01 14:31:19 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -3,7 +3,7 @@ import pymongo
 import os
 
 os.system("sudo mv pkmn.json ../../data/db")
-os.system("mongod -v --bind_ip_all --noauth --dbpath ../../data/db")
+os.system("mongod -v --bind_ip_all --noauth --dbpath ~/data/db")
 os.system("mongoimport --db YaThatWasABanana --collection pkmn --drop --file ../../data/db/pkmn.json")
 SERVER_ADDR = "206.81.13.98"
 connection = pymongo.MongoClient(SERVER_ADDR)
