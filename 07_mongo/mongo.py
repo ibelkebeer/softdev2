d7c805100844feaedcff8b1ce009ccbac7a6b10a
Imad <ibelkebir@stuy.edu>
2019-03-03 17:26:57 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -13,5 +13,5 @@ connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.YaThatWasABanana
 collection = db.pkmn
 
-pokemon = list(collection.find())
+pokemon = list(collection.find({'pokemon.name': 'Bulbasaur'}))
 print(pokemon)
