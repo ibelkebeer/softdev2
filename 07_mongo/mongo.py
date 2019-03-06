68196d5a954557a3024ef8debf12e9fd6ead2c1e
ibelkebeer <ibelkebir@stuy.edu>
2019-03-06 13:06:54 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -13,7 +13,7 @@ file into the database.
 import pymongo
 import os
 
-os.system("sudo mv pkmn.json ../../data/db")
+os.system("sudo mv pkmn.json ~/data/db")
 os.system("mongoimport --db YaThatWasABanana --collection pkmn --drop --file ~/data/db/pkmn.json")
 SERVER_ADDR = "206.81.13.98"
 connection = pymongo.MongoClient(SERVER_ADDR)
