e1d7067742238d586bc139144f95072f1d2199dd
ibelkebeer <ibelkebir@stuy.edu>
2019-05-02 12:46:00 -0400

    memo

---
+++ 23_memoize/memoize.py
@@ -0,0 +1,18 @@
+def make(f):
+    m = {}
+    def inner(x):
+        if x in m.keys():
+            return m[x]
+        m[x] = f(x)
+        return m[x]
+    return inner
+
+@make
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+print(fib(10))
